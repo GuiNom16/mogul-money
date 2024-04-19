bfda553a4fd4f3275406dbe57f3304b0ef087ff9
Guillaume Nombro <jeremie.nombro@accenture.com>
2024-04-19 12:03:33 +0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import {
-  Routes,
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-//import "./App.css";
-import SideMenu from "./layout/SideMenu/SideMenu";
-import Dashboard from "./layout/SideMenu/Test/Dashboard";
-import Home from "./pages/Home/Home";
-import Quiz from "./pages/Quiz/Quiz";
-import Results from "./pages/Results/Results";
-import { AppBar } from "@mui/material";
+import "./App.css";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<AppBar />}>
-        <Route path="Home" element={<Home />} />
+      <Route path="/" element={}>
+        <Route path="Home" element={} />
       </Route>
     )
   );
